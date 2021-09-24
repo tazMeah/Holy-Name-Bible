f1aa66089d9787e102b9287bd6cc7a124e528e86
Montaz Meah <montaz.meah@mrm.com>
2021-09-24 03:21:25 -0400

    added home button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
 import Book from "./components/Book";
 import "./App.css";
 import ScriptureProvider from "./context/ScriptureProvider";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Contents from "./components/Contents";
+import Nav from 'react-bootstrap/Nav'
 
 
 function App() {
@@ -12,21 +13,36 @@ function App() {
 	let bookName = searchParams.get("bookName"); 
 
 	return (
-		<Switch>
-			<Route path="/" exact>
-				<ScriptureProvider>
-					<div className="App">
-						<Contents />
-					</div>
-				</ScriptureProvider>
-			</Route>
-			{/* <Route path="/:bookNumber" exact>
+		<>
+			<Nav defaultActiveKey="/" as="ul">
+				<Nav.Item as="li">
+					<Nav.Link>
+						<NavLink to="/">Home</NavLink>
+					</Nav.Link>
+				</Nav.Item>
+				{/* <Nav.Item as="li">
+					<Nav.Link eventKey="link-1">Link</Nav.Link>
+				</Nav.Item>
+				<Nav.Item as="li">
+					<Nav.Link eventKey="link-2">Link</Nav.Link>
+				</Nav.Item> */}
+			</Nav>
+			<Switch>
+				<Route path="/" exact>
+					<ScriptureProvider>
+						<div className="App">
+							<Contents />
+						</div>
+					</ScriptureProvider>
+				</Route>
+				{/* <Route path="/:bookNumber" exact>
 				<Book bookName={bookName}/>
 			</Route> */}
-			<Route path="/:bookNumber/:chapterNumber" exact>
-				<Book bookName={decodeURI(bookName as string)}/>
-			</Route>
-		</Switch>
+				<Route path="/:bookNumber/:chapterNumber" exact>
+					<Book bookName={decodeURI(bookName as string)} />
+				</Route>
+			</Switch>
+		</>
 	);
 }
 
