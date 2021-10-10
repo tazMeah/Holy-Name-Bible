c0ceb0e2bef9e56e92beaa28c7a9626825b92b52
Montaz Meah <montaz.meah@mrm.com>
2021-10-10 16:13:49 -0400

    removed dropdown navigation. replaced it with google style links. Added previous and next buttons at the bottom of the page.

--- src/components/Book.tsx
+++ src/components/Book.tsx
@@ -1,9 +1,11 @@
-import { useParams, useHistory } from "react-router-dom";
-import { useLayoutEffect } from "react";
+import { useParams, useHistory, NavLink, Link } from "react-router-dom";
+import { useLayoutEffect, useState } from "react";
 import Bible from "../services/Bible";
 import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Form from "react-bootstrap/Form";
 import "./Book.css";
+import Button from "react-bootstrap/Button";
+import { booksOfTheBible } from "../models/BooksOfTheBible";
 
 interface params {
 	bookNumber: string;
@@ -47,10 +49,53 @@ export default function Book() {
 		window.onbeforeunload = null; // necessary to prevent infinite loop, that kills your browser 
 	}
 
+	// pagination links
+	let previousLink:string, nextLink:string;
+	// console.log(allChapters);
+	// nextLink
+	if (+chapterNumber < allChapters.length){
+		nextLink =`/${+bookNumber}/${+chapterNumber + 1}?bookName=${bookName}`;
+		// console.log("nextLink: ", nextLink)
+	} else if (!(+chapterNumber === 22 && +bookNumber === 66)) {
+		// console.log("chapterNumber: ", +chapterNumber);
+		// console.log("bookNumber: ", +bookNumber)
+		nextLink =`/${+bookNumber + 1}/1?bookName=${booksOfTheBible[+bookNumber].split(":")[1]}`;
+		// console.log("nextLink: ", nextLink);
+	}
+	// previousLink
+	const finalChapterNumbers:number[] = [50,40,27,36,34,24,21,4];
+
+	if (+chapterNumber > 1) {
+		previousLink = `/${+bookNumber}/${+chapterNumber - 1}?bookName=${bookName}`;
+		// console.log("previousLink: ", previousLink);
+	} else if(!(+chapterNumber === 1 && +bookNumber === 1)) {
+		previousLink = `/${+bookNumber - 1}/${finalChapterNumbers[+bookNumber - 2]}?bookName=${booksOfTheBible[+bookNumber - 2].split(":")[1]}`;
+		// console.log("previousLink: ", previousLink);
+	}
 	return (
 		<div className="book mt-3">
 			<h1>{bookName}</h1>
-			<FloatingLabel
+			<h5>Chapter: {chapterNumber}</h5>
+			<table id="chapters" className="mb-3">
+				<tbody>
+					{allChapters.map((chapter, index) => (
+						<tr key={index}>
+							<td className="chapterLinks">
+								<NavLink
+									to={
+										"/" + bookNumber + "/" + chapter + "?bookName=" + bookName
+									}
+									activeClassName="current"
+								>
+									{chapter}
+								</NavLink>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+
+			{/* <FloatingLabel
 				controlId="floatingSelect"
 				label="Chapter"
 				className="mb-3"
@@ -74,21 +119,32 @@ export default function Book() {
 						);
 					})}
 				</Form.Select>
-			</FloatingLabel>
+			</FloatingLabel> */}
 
-			{Bible.map((verse: {Book: number, Chapter: number, Verse: number, Scripture: string, Footnote?: string[]}, index) => {
-				return (
-					verse.Book === +bookNumber &&
-					verse.Chapter === +chapterNumber && (
-						<p key={index} id={"v" + verse.Verse}>
-							<span className="verseNumber" aria-hidden="true">
-								{verse.Chapter}:{verse.Verse}{" "}
-							</span>
-							&nbsp;&nbsp;{verse.Scripture}
-						</p>
-					)
-				);
-			})}
+			{Bible.map(
+				(
+					verse: {
+						Book: number;
+						Chapter: number;
+						Verse: number;
+						Scripture: string;
+						Footnote?: string[];
+					},
+					index
+				) => {
+					return (
+						verse.Book === +bookNumber &&
+						verse.Chapter === +chapterNumber && (
+							<p key={index} id={"v" + verse.Verse}>
+								<span className="verseNumber" aria-hidden="true">
+									{verse.Chapter}:{verse.Verse}
+								</span>
+								&nbsp;&nbsp;{verse.Scripture}
+							</p>
+						)
+					);
+				}
+			)}
 
 			<div className="footnotes">
 				{Bible.map((verse, index) => {
@@ -96,18 +152,29 @@ export default function Book() {
 					return (
 						verse.Book === +bookNumber &&
 						verse.Chapter === +chapterNumber &&
-						verse.Footnote && (
-							verse.Footnote.map((footnote, index) => {
+						verse.Footnote &&
+						verse.Footnote.map((footnote, index) => {
 							return (
-							<p key={index} className="footnote">
-								{footnote}
-							</p>
-							)
-
-							})
-						)
+								<p key={index} className="footnote">
+									{footnote}
+								</p>
+							);
+						})
 					);
 				})}
+			</div>
+
+			<div className="pagination">
+				{(+bookNumber === 1 && +chapterNumber === 1) || (
+					
+						<Button onClick={()=>{history.push(previousLink); window.scrollTo(0,0)}}>&lt; Previous</Button>
+					
+				)}{" "}
+				{(+bookNumber === 66 && +chapterNumber === 22) || (
+					
+						<Button onClick={() => {history.push(nextLink); window.scrollTo(0,0)}}>Next &gt;</Button>
+					
+				)}
 			</div>
 		</div>
 	);
